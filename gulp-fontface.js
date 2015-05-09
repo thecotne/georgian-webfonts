f313f6f04d9928ee24bc8615dc085818039d3642
cotne nazarashvili <cotne.nazarashvili@gmail.com>
2015-05-09 22:59:48 +0400

    here we go

---
+++ gulp-fontface.js
@@ -0,0 +1,31 @@
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+var opentype = require('opentype.js');
+var bufferToArrayBuffer = require('buffer-to-arraybuffer');
+
+const PLUGIN_NAME = 'gulp-fontface';
+
+function gulpFontface(callback) {
+	return through.obj(function (file, enc, cb) {
+		if (file.isBuffer()) {
+			var font = opentype.parse(bufferToArrayBuffer(file.contents));
+			console.log(font.tables.name.fontSubfamily);
+			var data = {
+				fullName: font.tables.name.fullName,
+				fontFamily: font.tables.name.fontFamily,
+				fontSubfamily: font.tables.name.fontSubfamily,
+				fontStyle: /italic/i.test(font.tables.name.fontSubfamily) ? "italic" : "normal",
+				fontWeight: /bold/i.test(font.tables.name.fontSubfamily) ? "bold" : "normal",
+				path: file.relative.replace(/\\/g, '/'),
+			};
+			callback(data, function() {
+				cb(null, file);
+			});
+		} else {
+			cb(null, file);
+		}
+	});
+}
+
+module.exports = gulpFontface;
---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var fontface = require('./gulp-fontface');
+var rename = require('gulp-rename');
+var consolidate = require('gulp-consolidate');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+var gcallback = require('gulp-callback');
+var header = require('gulp-header');
+var stripCssComments = require('gulp-strip-css-comments');
+
+var head = '/*\n\
+ * georgian-webfonts\n\
+ * https://github.com/thecotne/georgian-webfonts\n\
+ *\n\
+ * Copyright (c) 2015 cotne nazarashvili\n\
+ * Licensed under the MIT license.\n\
+ */\n\
+';
+
+gulp.task('fontface', function() {
+	return gulp.src('fonts/**/**.{ttf,otf}', {base: '.'})
+		.pipe(fontface(function(data, end) {
+			gulp
+				.src('template.css')
+				.pipe(consolidate('lodash', data))
+				.pipe(rename({basename: data.fullName}))
+				.pipe(header(head))
+				.pipe(gulp.dest('css'))
+				.pipe(gcallback(end))
+		}))
+});
+
+gulp.task('all', function() {
+	return gulp
+		.src('css/**.css')
+		.pipe(concat('all.css', {newLine: '\n'}))
+		.pipe(stripCssComments())
+		.pipe(header(head))
+		.pipe(gulp.dest('css'))
+		.pipe(minifyCss())
+		.pipe(header(head))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['fontface']);
